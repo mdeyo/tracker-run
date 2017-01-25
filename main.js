8647d8114803a786d06c5fff18c6327daf38ca3b
DEYO <mdeyo@mit.edu>
2017-01-24 23:22:35 -0500

    added add button to each day of the month with access to month+day

--- main.js
+++ main.js
@@ -14,14 +14,32 @@ function init() {
 
     var classname = document.getElementsByClassName("day");
 
-    var myFunction = function () {
-        console.log(this);
-        // var attribute = this.getAttribute("data-myattribute");
-        // alert(attribute);
+    var myFunction = function() {
+        // console.log(this);
+        // console.log(this.innerHTML);
+        console.log('date:' + this.getAttribute("date"));
+        var date = this.date;
+        var dateSpan = document.createElement('span')
+        dateSpan.innerHTML = this.innerHTML;
+        dateSpan.className += "active";
+        console.log(this.childNodes);
+
+        var button = this.getElementsByClassName('add-button')['button'];
+
     };
 
-    for (var i = 0; i < classname.length; i++) {
-        classname[i].addEventListener('click', myFunction, false);
+    var addFunction = function() {
+        console.log("button pressed");
+        var date = this.parentElement.getAttribute("month") + " " + this.parentElement.getAttribute("date");
+        console.log("date:" + date);
     }
+
+    for (var i = 0; i < classname.length; i++) {
+        // classname[i].addEventListener('click', myFunction, false);
+        if (classname[i].getElementsByClassName('add-button')['button']) {
+            classname[i].getElementsByClassName('add-button')['button'].addEventListener('click', addFunction, false);
+        }
+    }
+
 
 }
